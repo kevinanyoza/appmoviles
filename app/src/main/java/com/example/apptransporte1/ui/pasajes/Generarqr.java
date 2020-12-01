572c879086d7f8108e269fc67c9c842d3d4967fa
JEAN KEVIN ANYOZA CCENHUA <janyoza@senasa.gob.pe>
2020-12-01 12:56:44 -0500

    ultimos cambios pasajes
    listo

--- app/src/main/java/com/example/apptransporte1/ui/pasajes/Generarqr.java
+++ app/src/main/java/com/example/apptransporte1/ui/pasajes/Generarqr.java
@@ -1,66 +1,138 @@
 package com.example.apptransporte1.ui.pasajes;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.apptransporte1.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Generarqr#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Generarqr extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    CheckBox checkboxconfirmar;
+    EditText monto;
 
     public Generarqr() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Generarqr.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Generarqr newInstance(String param1, String param2) {
-        Generarqr fragment = new Generarqr();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generarqr, container, false);
+        View v = inflater.inflate(R.layout.fragment_generarqr, container, false);
+
+        Bundle bundle =  getActivity().getIntent().getExtras();
+        String id_usuario = getActivity().getIntent().getStringExtra("id_usuario");//este es el id del usuario iniciado
+
+        //Toast.makeText(getActivity(), "monto: "+ montototal, Toast.LENGTH_SHORT).show();
+
+        checkboxconfirmar = v.findViewById(R.id.checkboxconfirmar);
+        checkboxconfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MostrarAlerta(id_usuario);
+            }
+        });
+
+        return v;
     }
+
+    private void   MostrarAlerta(String id_usuario){
+        final EditText input = new EditText(getActivity());
+
+        AlertDialog dialog = (new AlertDialog.Builder(getContext()))
+                .setTitle("Confirmación"+id_usuario)
+                .setMessage("Esta seguro de realizar la compra:")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Navigation.findNavController(getView()).navigate(R.id.nav_pasajes);
+                        //actualizarSaldo(id_usuario);
+                        Toast.makeText( getActivity(), "Compra Realizada Satisfactoriamente :)", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .create();
+        //ialog.setView(input, (int)(19*dpi), (int)(5*dpi), (int)(14*dpi), (int)(5*dpi) );
+        dialog.show();
+
+        //alertDialog.show();
+    }
+
+    private void  actualizarSaldo(String id_usuario){
+        String url = "http://aplicacionanyoza.atwebpages.com/index.php/usuario/recarga/origen";
+        String montoimpor = monto.getText().toString();
+        String usuario = id_usuario;
+        StringRequest peticion = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    Toast.makeText(getActivity(), "Transferencia correcta", Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        },  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        }){
+
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("idUsuario", usuario );
+                params.put("saldo", ""+ montoimpor);
+                return params;
+            }};
+
+        RequestQueue cola = Volley.newRequestQueue(getActivity());
+        cola.add(peticion);
+
+    }
+
+
+    private void actualizaMontoOrigen (String monto){
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/apptransporte1/ui/pasajes/nav_pasajes.java
+++ app/src/main/java/com/example/apptransporte1/ui/pasajes/nav_pasajes.java
@@ -21,14 +21,17 @@ import android.widget.TextView;
 
 import com.example.apptransporte1.MainActivity;
 import com.example.apptransporte1.R;
+import com.example.apptransporte1.RecargarFragment;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class nav_pasajes extends Fragment {
 
     Button btngenerarqr;
-    TableLayout menutabcorredor, menutabtren,menutabmetropoli, tableLayout;
+    TabLayout menutabcorredor, menutabtren,menutabmetropoli, tableLayout;
     TextView txtpreuni;
-    Spinner spcantidad;
+    Spinner spcantidad , spdestino,sppartida;
+
     EditText multimonto;
 
     public nav_pasajes() {
@@ -64,37 +67,51 @@ public class nav_pasajes extends Fragment {
         txtpreuni = view.findViewById(R.id.txtpreunitario);
         spcantidad = view.findViewById(R.id.spcantidad);
         multimonto = view.findViewById(R.id.multxtmonto);
+        tableLayout = view.findViewById(R.id.tabLayout);
+        spdestino = view.findViewById(R.id.spdestino);
+        sppartida = view.findViewById(R.id.sppartida);
 
+        spdestino.setEnabled(false);
+        sppartida.setEnabled(false);
 
-
-    /*    menutabtren.setOnClickListener(new View.OnClickListener() {
+        tableLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                txtpreuni.setText("1.50");
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+
+                    txtpreuni.setText("2.50");
+                    calularMonto();
+                    spdestino.setEnabled(false);
+                    sppartida.setEnabled(false);
+
+                } else if (tab.getPosition() == 1) {
+                    txtpreuni.setText("1.50");
+                    calularMonto();
+                    spdestino.setEnabled(false);
+                    sppartida.setEnabled(false);
+                } else {
+                    spdestino.setEnabled(true);
+                    sppartida.setEnabled(true);
+                    txtpreuni.setText("1.50");
+                    calularMonto();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
-
-        menutabmetropoli.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txtpreuni.setText("2.50");
-            }
-        });*/
 
         spcantidad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String cant = spcantidad.getSelectedItem().toString();
-                String prec = txtpreuni.getText().toString();
-                double cantidad, precio, total;
-                String importe;
-
-                cantidad = Double.parseDouble(cant);
-                precio = Double.parseDouble(prec);
-
-                total = cantidad * precio;
-                //importe =
-                multimonto.setText(Double.toString(total));
+            calularMonto();
             }
 
             @Override
@@ -107,9 +124,36 @@ public class nav_pasajes extends Fragment {
         btngenerarqr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                //intent.putExtra("monto_total", "ejemplito" );
+                //Fragment generar = new Generarqr();
+
+                /*Bundle args = new Bundle();
+                args.putString("monto_total", "ejemplo");
+                generar.setArguments(args);*/
+
                 Navigation.findNavController(v).navigate(R.id.generarqr);
+
             }
         });
 
     }
+
+    private void calularMonto(){
+        String cant = spcantidad.getSelectedItem().toString();
+        String prec = txtpreuni.getText().toString();
+        double cantidad, precio, total;
+
+        cantidad = Double.parseDouble(cant);
+        precio = Double.parseDouble(prec);
+
+        total = cantidad * precio;
+
+        final String importe = Double.toString(total);
+        //Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+        //intent.putExtra("monto_total", "ejemplito" )
+
+        multimonto.setText(Double.toString(total));
+    }
+
 }
